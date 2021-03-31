5f9b0e755cf7c8305f7a6e9aa2bdd1fa2ed182be
SteavnChen <tom9876555@yahoo.com.tw>
2021-03-31 21:39:03 +0800

    安裝mongoose,並設定資料庫連線,首頁排版

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/expense-tracker', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+} )
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars',exphbs({ defaultLayout: 'main'}) )
 app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/new', (req, res) => {
+   res.render('new')
+})
+
 app.listen(3000, () => {
-  console.log('express is running')
+  console.log('App is running on http://localhost:3000')
 })
